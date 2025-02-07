473078e3f576b7fe6748f161263e54a2bb59346b
Emir Özder <emir.ozder@id3.com.tr>
2025-02-07 23:17:48 +0300

    feat: init project
    feat: backend init structure
    
    feat: init frontend

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export const signup = (req, res) => {
+  res.send("Sign up route");
+}
+
+export const login = (req, res) => {
+  res.send("Log in route");
+}
+
+export const logout = (req, res) => {
+  res.send("Log out route");
+}
---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+      <div>MERN Chat App</div>
+    </>
+  );
+}
+
+export default App;
